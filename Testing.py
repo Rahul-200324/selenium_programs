09ac453fd6a07187aa682a983e5b72cd09d66ceb
Rahul-200324 <rahulahiregoudar24@gmail.com>
2026-06-16 12:24:44 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,1 +1,14 @@
 print("Hello World")
+name=input("Enter the name of the student")
+
+if (name == "Rahul"):
+  last_name= input("Enter the Lastname of the student : ")
+  if (last_name == "Hiregoudar"):
+    usn=input("Enter the USN of the student")
+    if (usn == "2BL22EE050"):
+      branch=input("Enter the branch of the student")
+      if (branch == "EEE"):
+        schem=int(input("Inter the Schem of year"))
+        if (schem == 2022):
+          print("Name : Rahul A Hiregoudar \n USN : 2BL22EE050 \n Branch : Electrical and Electronics Engineering \n CGPA : 7.8 ")
+          
